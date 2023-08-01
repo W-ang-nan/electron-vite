c64a906edf09d91009e2bf2fc6686a2209b698d8
wangnan <wangnan_yx@163.com>
2023-08-01 16:10:38 +0800

    同步去除out

--- out/renderer/assets/Station-fbb34a52.js
+++
@@ -1,9 +0,0 @@
-import { _ as _export_sfc, f as openBlock, h as createElementBlock } from "./index-3d9a4c14.js";
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache) {
-  return openBlock(), createElementBlock("div", null, " 电台 ");
-}
-const Station = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
-export {
-  Station as default
-};
--- out/renderer/assets/artist-d69c17e7.js
+++
@@ -1,6 +0,0 @@
-import { _ as _sfc_main } from "./artist.vue_vue_type_style_index_0_lang-44eb2371.js";
-import "./index-3d9a4c14.js";
-import "./artist-9fa04616.js";
-export {
-  _sfc_main as default
-};
